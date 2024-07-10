8e054cb804033cafb5d151bad3814fa9086a7119
FedFedavi <kykypyk@gmail.com>
2024-07-10 19:55:14 +0400

    add new class

---
+++ main.py
@@ -0,0 +1,28 @@
+class Warrior():
+
+    def __init__(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
+
+    def sleep(self):
+        print(f"{self.name} лег спать")
+        self.endurance += 2
+
+    def eat(self):
+        print(f"{self.name} сел поесть")
+        self.power += 1
+
+    def hit(self):
+        print(f"{self.name} бьет когото")
+        self.endurance -= 6
+
+    def walk(self):
+        print(f"{self.name} гуляет")
+
+    def info(self):
+        print(f"имя воина {self.name}")
+        print(f"сила воина {self.power}")
+        print(f"выносливость воина {self.endurance}")
+        print(f"цвет волос воина {self.hair_color}")
