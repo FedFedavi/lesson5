2c0f5e2304c19a9b4405382b44842b938c58c0fb
FedFedavi <kykypyk@gmail.com>
2024-07-11 20:56:32 +0400

    new code Bank dep

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-class Warrior():
+class Account():
+    def __init__(self, id, balance=0, ):
+        self.id = id
+        self.balance = balance
 
-    def __init__(self, name, power, endurance, hair_color):
-        self.name = name
-        self.power = power
-        self.endurance = endurance
-        self.hair_color = hair_color
+    def deposit(self, money):
+        if money > 0:
+            self.balance += money
+            print(f"Вы успешно пополнили счет. Сумма на счету {self.balance}")
 
-    def sleep(self):
-        print(f"{self.name} лег спать")
-        self.endurance += 2
 
-    def eat(self):
-        print(f"{self.name} сел поесть")
-        self.power += 1
+    def withdraw(self, money):
+        if self.balance >= money:
+            self.balance -= money
+            print(f"Вы успешно сняли {money}. Баланс {self.balance}")
+        else:
+            print(f"Денежный средств недостаточно. Вы запросили {money}, а на счете {self.balance}")
 
-    def hit(self):
-        print(f"{self.name} бьет когото")
-        self.endurance -= 6
+    def all_balance(self):
+        print(f"Текущий баланс - {self.balance}")
 
-    def walk(self):
-        print(f"{self.name} гуляет")
 
-    def info(self):
-        print(f"имя воина {self.name}")
-        print(f"сила воина {self.power}")
-        print(f"выносливость воина {self.endurance}")
-        print(f"цвет волос воина {self.hair_color}")
+man = Account("1234567", 600)
 
-war1 = Warrior("Степа", 76, 54, "коричневый")
-war2 = Warrior("Егор", 45, 23, "блонд")
-
-war1.info()
-war1.sleep()
-war1.eat()
-war1.hit()
-war1.walk()
-war1.info()
-
-war2.info()
-war2.sleep()
-war2.eat()
-war2.hit()
-war2.walk()
-war2.info()
-
-sleep()
+man.all_balance()
+man.withdraw(300)
+man.withdraw(800)
+man.deposit(23000)
+man.all_balance()
